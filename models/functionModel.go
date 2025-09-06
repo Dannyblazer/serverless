1a19cda58463a9f1b7cca66b730ae8c81d3d5793
Dannyblazer <nnajidanny00@gmail.com>
2025-09-06 14:39:18 +0100

    Account model to Function App Integration

--- models/functionModel.go
+++ models/functionModel.go
@@ -4,9 +4,17 @@ import (
 	"gorm.io/gorm"
 )
 
+type Account struct {
+	gorm.Model
+	Email        string `gorm:"unique"`
+	Password     string
+	FunctionApps []FunctionApp `gorm:"foreignKey:AccountID; constraint:OnDelete:CASCADE"`
+}
+
 type FunctionApp struct {
 	gorm.Model
-	Name string `gorm:"unique"`
-	Path string
-	Size int64
+	Name      string `gorm:"unique"`
+	Path      string
+	Size      int64
+	AccountID uint
 }
